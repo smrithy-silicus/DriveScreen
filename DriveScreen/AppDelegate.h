1ef99ea86ce0fee5f67de6494eaf2734ffe0f701
smrithy-silicus <smrithyrvarma@SIL-MAC-04.local>
2017-07-31 15:08:48 +0530

    SV - Adding initial set of files for drive screen.

---
+++ DriveScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DriveScreen
+//
+//  Created by smrithy r varma on 31/07/17.
+//  Copyright © 2017 Silicus Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DriveScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DriveScreen
+//
+//  Created by smrithy r varma on 31/07/17.
+//  Copyright © 2017 Silicus Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
